d638ab19dfadee85f9d2699d7decaef529b074da
Thomas Luechtefeld <tom@insilica.co>
2023-03-15 20:59:13 -0400

    not using template stages

---
+++ stages/download.R
@@ -0,0 +1,12 @@
+
+# Download invitrodb.csv and documentation from Clowder 
+# documentation available at https://clowder.edap-cluster.com/files/62cee26ee4b055edffbba11d?dataset=62c435c7e4b01d27e3b2b61f&space=62bb560ee4b07abf29f88fef
+stage = fs::dir_create("staging") |> fs::path("invitrodb.csv")
+invitrodb = "https://clowder.edap-cluster.com/files/63642290e4b04f6bb140a10d/blob"
+
+options(timeout = 600)
+download.file(invitrodb, destfile = stage, mode = "wb")
+
+df = readr::read_csv(stage)
+out = fs::dir_create("brick") |> fs::path("invitrodb.parquet")
+arrow::write_parquet(df, out)
